dbca2b92fa82f03ee01b5afc378e39ae01fb43af
Ola Ellnestam <ola@agical.se>
2014-05-09 08:31:13 +0200

    Almost able to log client calls

--- server/lib/launcher.js
+++ server/lib/launcher.js
@@ -4,6 +4,8 @@ var fs = require('fs');
 var q = require('q');
 var opt = require('optimist');
 var http = require('http');
+var url = require('url');
+var client = require('./logger/logger.js');
 
 var port = 7331;
 
@@ -13,12 +15,16 @@ var srv = http.createServer(function (req, res) {
     var search = parsed.search;
     
     var message = parsed.pathname.split('/');
-    console.log(req.url);
-    
-    console.log(message);
-    
-    if (url.pathname === 'account') {
-	res.end('Account\n');
+    console.log("S: " + search);
+
+    var action = message[1];
+    if (action === 'account') {
+	client.store('Hello');
+	console.log('Account!');
+	res.end('[account]: Message parsed OK.\n');
+    } else if (action === 'loan') {
+	console.log('Loan!');
+	res.end('[loan]: Message parsed OK.\n');
     }
     
     res.end('Unrecognized command\n');
---
+++ server/lib/logger/logger.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var logger = {
+
+    store : function(message) {
+	console.log(message);
+    }
+
+};
+
+
+module.exports = logger;
