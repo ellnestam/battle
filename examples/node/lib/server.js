3868ef1cbda0c37395dbe77c20562fc3d75377d3
Tobias Anderberg <tobias.anderberg@agical.com>
2014-10-10 15:18:55 +0200

    Super simple example server implemented

--- examples/node/lib/server.js
+++ examples/node/lib/server.js
@@ -1,19 +1,40 @@
 var http = require('http');
-var faye = require('faye');
 var url = require('url');
 
 var port = 7331;
 
 var srv = http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var parsed = url.parse(req.url);
-    var search = parsed.search;
-    
-    var message = parsed.pathname.split('/');
-    
-    if (url.pathname === 'account') {
-      res.end(url.id + '\n');
-    }
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  var parsed = url.parse(req.url, true);
+  var query = parsed.query;
+  
+  var message = parsed.pathname.split('/');
+  
+  var action = message[1];
+  if (action === 'account') {
+    console.log('Account!');
+    res.end(query.id);
+  } else if (action === 'loan') {
+    console.log('Loan!');
+    res.end(query.id);
+  }
+  
+  res.end('Unrecognized command\n');
+});
 
-    res.end('Unrecognized command\n');
-}).listen(port);
+var launcher = {
+    run : function launcher(args) {
+	if (args.length < 1) {
+	    console.log('');
+	    console.log("Usage: node launcher.js");
+	    console.log('');
+	} else {
+	    console.log('The Bank is open for business');
+	    srv.listen(port)
+	};
+    }
+}
+
+var srv = launcher.run(process.argv);
+
+module.exports = launcher;
